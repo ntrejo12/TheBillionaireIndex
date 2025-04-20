fb178b2b6b5a73357474f15c8e802aadb314f0bb
Yusra89hash <yusra9189@gmail.com>
2025-04-20 02:59:23 -0400

    updated billiionaire class (now includes parsing through the entire dataset) and merge sort,

--- billionaire.h
+++ billionaire.h
@@ -4,58 +4,108 @@
 #ifndef MERGE_H
 #define MERGE_H
 #endif //MERGE_H
-
+#include <sstream>
+#include <fstream>
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
 class Billionaire {
 private:
     string name;
-    int year;
-    double networth;
-    char gender;
-    int age;
+    string year;
+    string networth;
+    string age;
     string company;
     string country;
 public:
-    Billionaire(string name, int year, double networth, char gender, int age, string company, string country) {
+    Billionaire() {
+        name = "";
+        year = "";
+        networth = "";
+        age = "";
+        company = "";
+        country = "";
+    }
+    Billionaire(string name, string year, string networth, string age, string company, string country) {
         this->name = name;
         this->year = year;
         this->networth = networth;
-        this->gender = gender;
         this->age = age;
         this->company = company;
         this->country = country;
     }
-    string getName() {
+    string getName() const{
         return name;
     }
-    int getYear() {
+    string getYear() const{
         return year;
     }
-    double getNetworth() {
+    string getNetworth() const{
         return networth;
     }
-    char getGender() {
-        return gender;
-    }
-    int getAge() {
+
+    string getAge() const{
         return age;
     }
-    string getCompany() {
+    string getCompany() const{
         return company;
     }
-    string getCountry() {
+    string getCountry() const{
         return country;
     }
     void display() {
         cout << "Name: " << getName() << endl;
         cout << "Year: " << getYear() << endl;
         cout << "Networth: " << getNetworth() << endl;
-        cout << "Gender: " << getGender() << endl;
         cout << "Age: " << getAge() << endl;
         cout << "Company: " << getCompany() << endl;
         cout << "Country: " << getCountry() << endl;
     }
+
+    bool operator<=(const Billionaire &other) {
+        return this->year <= other.year;
+    }
+    static vector<Billionaire> readFromFile(string filename) {
+        int count = 0;
+        vector<Billionaire> billionaireList;
+        ifstream file(filename);
+        if (!file.is_open()) {
+            cerr << "Error opening file: " << filename << endl;
+        }
+        string line;
+        string temp, tempName, tempYear, tempNetworth, tempAge, tempCompany, tempCountry;
+        while (getline(file, line)) {
+            count++;
+            if (count >= 2) {
+                stringstream ss(line);
+                getline(ss, temp, ',');
+                getline(ss, tempName, ',');
+                getline(ss, temp, ',');
+                getline(ss, tempAge, ',');
+                getline(ss, temp, ',');
+                getline(ss, tempYear, ',');
+                getline(ss, tempCountry, ',');
+                getline(ss, tempCompany, ',');
+                getline(ss, temp, ',');
+                getline(ss, temp, ',');
+                getline(ss, temp, ',');
+                getline(ss, temp, ',');
+                getline(ss, temp, ',');
+                getline(ss, temp, ',');
+                getline(ss, tempNetworth, ',');
+                getline(ss, temp, ',');
+                getline(ss, temp, ',');
+                getline(ss, temp, ',');
+
+                Billionaire tempBillionaire(tempName, tempYear, tempNetworth, tempAge, tempCompany, tempCountry);
+                billionaireList.push_back(tempBillionaire);
+            }
+        }
+        return billionaireList;
+    }
+
+
+
 };
---
+++ merge.cpp
@@ -0,0 +1,71 @@
+#include "billionaire.h"
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+//code from geeksforgeeks
+//https://www.geeksforgeeks.org/merge-sort/
+
+void merge(vector<Billionaire>& data, int left, int mid, int right) {
+
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+    vector<Billionaire> L(n1), R(n2);
+
+    for (int i = 0; i < n1; i++) {
+        L[i] = data[i + left];
+    }
+    for (int i = 0; i < n2; i++) {
+        R[i] = data[i + mid + 1];
+    }
+    int i = 0, j = 0;
+    int k = left;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            data[k] = L[i];
+            i++;
+        }
+        else {
+            data[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < n1) {
+        data[k] = L[i];
+        i++;
+        j++;
+    }
+    while (j < n2) {
+        data[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(vector<Billionaire>& data, int left, int right) {
+    if (left >= right) {
+        return;
+    }
+    int mid = left + (right - left) / 2;
+    mergeSort(data, left, mid);
+    mergeSort(data, mid + 1, right);
+    merge(data, left, mid, right);
+}
+
+
+
+int main() {
+    cout << "Hello, World!" << endl;
+    string filename = "/Users/yusrahash/Downloads/billionaire_list_20yrs.csv";
+    cout << "1" << endl;
+    vector<Billionaire> n = Billionaire::readFromFile(filename);
+    for (int i = 0; i < n.size(); i++) {
+        n[i].display();
+    }
+    cout << "2" << endl;
+
+    return 0;
+}
